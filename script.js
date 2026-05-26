5dd9a9bb44c13db55d2cd4483ace283e9003e9f4
pushpa1011 <105695742+pushpa1011@users.noreply.github.com>
2026-05-26 15:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function stringChop(str, size) {
-  // your code here
+  // Convert size to number
+  size = Number(size);
+
+  // Edge cases
+  if (!str) return [];
+  if (!size || size <= 0) return [];
+
+  let result = [];
+
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.substring(i, i + size));
+  }
+
+  return result;
 }
 
 // Do not change the code below
